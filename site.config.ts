a297b2b7bd24e406f61c2af444a475f33819d56b
luckshim <shimpark@gmail.com>
2024-06-13 01:26:44 +0900

    초기화

--- site.config.ts
+++ site.config.ts
@@ -2,7 +2,7 @@ import { siteConfig } from './lib/site-config';
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: 'd1e89e9e42eb4ebf9486ae0374039efc',
+  rootNotionPageId: '2a51573987054c8cb016242b4b9c3952',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
@@ -53,12 +53,12 @@ export default siteConfig({
   navigationStyle: 'custom',
   navigationLinks: [
     {
-      title: '카테고리',
-      pageId: '36400db511474331b5c1de6918212469',
+      title: '목록',
+      pageId: '2a51573987054c8cb016242b4b9c3952',
     },
     {
-      title: '카테고리',
-      pageId: '36400db511474331b5c1de6918212469',
+      title: '대학내일',
+      pageId: '2a51573987054c8cb016242b4b9c3952',
       menuPage: true,
     },
   ],
